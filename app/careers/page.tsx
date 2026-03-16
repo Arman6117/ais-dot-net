3ef164c9467deb02853b49fb00637403fde4a4f9
Arman Patel <vanixgaming634@gmail.com>
2026-03-16 13:24:12 +0530

    refactor:Added footer

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -2,7 +2,7 @@ import CareersHero from "@/components/careers/careers-hero";
 import HowItWorks from "@/components/careers/how-it-works";
 import TermsSection from "@/components/careers/terms-section";
 import TestimonialsSection from "@/components/ui/about/testimonials-section";
-
+import Footer from "@/components/ui/footer"
 
 export default function CareersPage() {
   return (
@@ -13,7 +13,7 @@ export default function CareersPage() {
       {/* <InternshipDomains /> */}
       <TermsSection />
       <TestimonialsSection />
-        
+        <Footer/>
     </main>
   );
 }
